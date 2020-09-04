b362a7e4b3cf86e54aeb3aa4f279aed1aae526bd
Dylan Lyu <mingzong.lyu@gmail.com>
2020-09-04 22:06:35 +0800

    註解

--- main.go
+++ main.go
@@ -9,14 +9,19 @@ type Product struct {
 }
 
 func main() {
+	//建立Product庫
 	var products []Product
+	//建立CRUD操作物件
 	var product Product
+	//建立第一個物品
 	product = Product{
 		Id: 1,
 		name: "衣服",
 		price: 80,
 	}
+	//新增一個
 	products = add(product,products)
+	//健立第二個物品
 	product = Product{
 		Id: 2,
 		name: "鞋子",
@@ -24,11 +29,13 @@ func main() {
 	}
 	products = add(product,products)
 	fmt.Println(products[1].name)
+	//更改第二個物品的名字
 	product = Product{
 		Id: 2,
 		name: "白白的鞋子",
 		price: 10000,
 	}
+	//更改
 	products = edit(product,products)
 	fmt.Println(products[1].name)
 
